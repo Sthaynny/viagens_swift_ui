5e6eba85ce41bb0b118235368bffef1840d1aaad
Igor Sthanny <igor.nascimento@popcode.com.br>
2021-11-21 16:59:49 -0300

    refactor: layout para ipad da lista

--- viagens_swift_ui/components/ListaViagensView.swift
+++ viagens_swift_ui/components/ListaViagensView.swift
@@ -8,17 +8,26 @@
 import SwiftUI
 
 struct ListaViagensView: View {
+    @Environment(\.horizontalSizeClass) var _horizontalSizeClass
+    var _isCompact: Bool{
+        return self._horizontalSizeClass == .compact
+    }
     var body: some View {
         List(viagens){ viagem in
             VStack(alignment: .leading){
                 Text(viagem.titulo)
+                    .font(.custom("Avenir", size: _isCompact ? 14 : 24))
                 Image(viagem.imagem)
                     .resizable()
-                    .frame(height: 125)
+                    .aspectRatio(contentMode: .fill)
+                    .frame(height:  _isCompact ? 125 : 300)
+                    .clipped()
                 HStack{
                     Text(viagem.quantidadeDeDias)
+                        .font(.custom("Avenir", size: _isCompact ? 14 : 24))
                     Spacer()
                     Text(viagem.valor)
+                        .font(.custom("Avenir", size: _isCompact ? 14 : 24))
                 }
             }.padding(.bottom, 10)
         }
@@ -27,6 +36,13 @@ struct ListaViagensView: View {
 
 struct ListaViagensView_Previews: PreviewProvider {
     static var previews: some View {
-        ListaViagensView()
+        Group{
+            ListaViagensView()
+                .environment(\.horizontalSizeClass, .compact)
+                .previewLayout(.fixed(width: 400, height: 800))
+            ListaViagensView()
+                .environment(\.horizontalSizeClass, .regular)
+                .previewLayout(.fixed(width: 900 , height: 900))
+        }
     }
 }
