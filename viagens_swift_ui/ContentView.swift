6d5a0e1b2575eaa6175d9214103154203fc91b42
Igor Sthanny <igor.nascimento@popcode.com.br>
2021-11-21 13:42:38 -0300

    feat: criando projeto

---
+++ viagens_swift_ui/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  viagens_swift_ui
+//
+//  Created by Igor Sthaynny on 21/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!\nPrimeira aprendizado com SwiftUi")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ viagens_swift_ui/viagens_swift_uiApp.swift
@@ -0,0 +1,17 @@
+//
+//  viagens_swift_uiApp.swift
+//  viagens_swift_ui
+//
+//  Created by Igor Sthaynny on 21/11/21.
+//
+
+import SwiftUI
+
+@main
+struct viagens_swift_uiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
